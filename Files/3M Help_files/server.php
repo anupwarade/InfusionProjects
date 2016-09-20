39185a21c0ea46b8b5c55b42f092cdcb7362fa8a
Joseph Liu CW <A6GRDZZ@usac.mmm.com>
2016-09-20 14:56:41 -0500

    Added html file

---
+++ Files/3M Help_files/server.php
@@ -0,0 +1,61 @@
+var lz_poll_server = "http://3mchat01.mmm.com/livezilla/";
+var lz_poll_url = "http://3mchat01.mmm.com/livezilla/server.php";
+var lz_poll_website = "";
+var lz_poll_frequency = 31536000;
+var lz_poll_file_chat = "chat.php";
+var lz_window_width = "500";
+var lz_window_height = "760";
+
+var lz_area_code = "";
+var lz_user_name = "";
+var lz_user_email = "ai1saXUyLmN3QG1tbS5jb20_";
+var lz_user_company = "IC0gSm9zZXBoIExpdSAtIEVOIC0gRW5nbGlzaCAoVW5pdGVkIFN0YXRlcyk_";
+var lz_user_question = "";
+var lz_user_phone = "RU4gLSBFbmdsaXNoIChVbml0ZWQgU3RhdGVzKQ__";
+var lz_user_customs = new Array('','','','','','','','','','');
+var lz_user_language = "";
+var lz_user_header = "";
+var lz_user_website = "";
+
+var lz_getp_track = "fbpos=MDI_&fbml=LTEw&fbmt=MTI_&fbmr=MA__&fbmb=MA__&fbw=MTYw&fbh=NDA_&fboo=MQ__";
+var lz_getp_chat = "ec=IC0gSm9zZXBoIExpdSAtIEVOIC0gRW5nbGlzaCAoVW5pdGVkIFN0YXRlcyk_&ee=ai1saXUyLmN3QG1tbS5jb20_&ep=RU4gLSBFbmdsaXNoIChVbml0ZWQgU3RhdGVzKQ__&nct=MQ__&hfk=MQ__";
+var lz_alert_html = 'PGRpdiBpZD0ibHpfY2hhdF9kaWFsb2ciIHN0eWxlPSJwYWRkaW5nOjAgMjBweDsiPg0KICAgIDx0YWJsZSBzdHlsZT0iaGVpZ2h0OjEwMCU7d2lkdGg6MTAwJTsiPg0KICAgICAgICA8dHI+DQogICAgICAgICAgICA8dGQgc3R5bGU9InRleHQtYWxpZ246Y2VudGVyO3ZlcnRpY2FsLWFsaWduOiBtaWRkbGU7Ij4NCiAgICAgICAgICAgICAgICA8ZGl2IGlkPSJsel9jaGF0X2RpYWxvZ19ib3giPg0KICAgICAgICAgICAgICAgICAgICA8ZGl2IGlkPSJsel9jaGF0X2RpYWxvZ19oZWFkZXIiPiZuYnNwOzNtY2hhdDAxLm1tbS5jb208L2Rpdj4NCiAgICAgICAgICAgICAgICAgICAgPGRpdiBpZD0ibHpfY2hhdF9kaWFsb2dfdGV4dF9mcmFtZSI+DQogICAgICAgICAgICAgICAgICAgICAgICA8ZGl2IGlkPSJsel9jaGF0X2RpYWxvZ190ZXh0Ij48L2Rpdj4NCiAgICAgICAgICAgICAgICAgICAgPC9kaXY+DQoNCiAgICAgICAgICAgICAgICAgICAgPGRpdiBpZD0ibHpfY2hhdF9kaWFsb2dfcmVzb3VyY2VfZnJhbWUiPg0KICAgICAgICAgICAgICAgICAgICAgICAgPGlmcmFtZSBpZD0ibHpfY2hhdF9kaWFsb2dfcmVzb3VyY2UiIHNyYz0iIj48L2lmcmFtZT4NCiAgICAgICAgICAgICAgICAgICAgPC9kaXY+DQoNCiAgICAgICAgICAgICAgICAgICAgPGZpZWxkc2V0IGlkPSJsel9jaGF0X2ZpbGVfZnJhbWUiIHN0eWxlPSJkaXNwbGF5Om5vbmU7bWFyZ2luOjIwcHg7Ij4NCiAgICAgICAgICAgICAgICAgICAgICAgIDxsZWdlbmQ+DQogICAgICAgICAgICAgICAgICAgICAgICAgICAgPGxhYmVsPjwhLS1sYW5nX2NsaWVudF9zZW5kX2ZpbGUtLT4mbmJzcDs8L2xhYmVsPg0KICAgICAgICAgICAgICAgICAgICAgICAgPC9sZWdlbmQ+DQogICAgICAgICAgICAgICAgICAgICAgICA8aWZyYW1lIGlkPSJsel9jaGF0X2ZpbGVfdXBsb2FkX2ZyYW1lIj48L2lmcmFtZT4NCiAgICAgICAgICAgICAgICAgICAgPC9maWVsZHNldD4NCg0KICAgICAgICAgICAgICAgICAgICA8ZGl2IGlkPSJsel9jaGF0X2RpYWxvZ19mb290ZXIiPg0KICAgICAgICAgICAgICAgICAgICAgICAgPGlucHV0IHR5cGU9ImJ1dHRvbiIgY2xhc3M9Imx6X2Zvcm1fYnV0dG9uIiBpZD0ibHpfY2hhdF9kaWFsb2dfcHJpbnQiIHZhbHVlPSI8IS0tbGFuZ19jbGllbnRfcHJpbnQtLT4iIG9uY2xpY2s9ImRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdsel9jaGF0X2RpYWxvZ19yZXNvdXJjZScpLmNvbnRlbnRXaW5kb3cucHJpbnQoKTsiPg0KICAgICAgICAgICAgICAgICAgICAgICAgPGlucHV0IHR5cGU9ImJ1dHRvbiIgY2xhc3M9Imx6X2Zvcm1fYnV0dG9uIiBpZD0ibHpfY2hhdF9kaWFsb2dfYnV0dG9uIiB2YWx1ZT0iT0siPg0KICAgICAgICAgICAgICAgICAgICA8L2Rpdj4NCiAgICAgICAgICAgICAgICA8L2Rpdj4NCiAgICAgICAgICAgIDwvdGQ+DQogICAgICAgIDwvdHI+DQogICAgPC90YWJsZT4NCjwvZGl2Pg0KPGRpdiBpZD0ibHpfY2hhdF9kaWFsb2dfYmciPjwvZGl2Pg0K';
+var lz_is_ie = false;
+
+var lz_overlay_chat_available = false;
+var lz_overlays_possible = true;
+var lz_direct_login = false;
+var lz_geo_error_span = 30;
+var lz_geo_data_count = 6;
+var lz_geo_resolution = null;
+var lz_geo_resolution;
+var lz_geo_resolution_needed = true;
+var lz_user_id = "8faf40b87d";
+var lz_browser_id = "929d08e9cc";
+var lz_server_id = "f6250";
+var lz_geo_url = "https://ssl.livezilla.net/geo/resolute/?aid=&sid=ZTJkMGUyMDk3ZjYwNDNlYQ==&dbp=1";
+var lz_mip = "169.12.44.xxx";
+var lz_oak = '';
+var lz_is_mobile = false;
+
+var chars = new Array('c','6','e','6','e','3','8','4','8','9','4','8','e','5','8','1','9','4','3','2','a','1','c','5','6','a','6','3','0','b','7','3','5','5','e','9','1','f','5','2',0);
+var order = new Array(31,6,39,18,13,5,7,15,12,16,19,37,24,38,0,1,20,36,4,27,10,2,30,11,23,35,32,22,29,9,33,34,26,17,3,8,21,14,28,25,0);
+while(lz_oak.length < (chars.length-1))for(var f in order)if(order[f] == lz_oak.length)lz_oak += chars[f];
+
+
+var lz_resources = new Array(false,false,false,false,false,false);
+LazyLoad=function(x,h){function r(b,a){var c=h.createElement(b),d;for(d in a)a.hasOwnProperty(d)&&c.setAttribute(d,a[d]);return c}function k(b){var a=i[b],c,d;if(a){c=a.callback;d=a.urls;d.shift();l=0;if(!d.length){c&&c.call(a.context,a.obj);i[b]=null;j[b].length&&m(b)}}}function w(){if(!e){var b=navigator.userAgent;e={async:h.createElement("script").async===true};(e.webkit=/AppleWebKit\//.test(b))||(e.ie=/MSIE/.test(b))||(e.opera=/Opera/.test(b))||(e.gecko=/Gecko\//.test(b))||(e.unknown=true)}}function m(b,
+a,c,d,s){var n=function(){k(b)},o=b==="css",f,g,p;w();if(a){a=typeof a==="string"?[a]:a.concat();if(o||e.async||e.gecko||e.opera)j[b].push({urls:a,callback:c,obj:d,context:s});else{f=0;for(g=a.length;f<g;++f)j[b].push({urls:[a[f]],callback:f===g-1?c:null,obj:d,context:s})}}if(!(i[b]||!(p=i[b]=j[b].shift()))){q||(q=h.head||h.getElementsByTagName("head")[0]);a=p.urls;f=0;for(g=a.length;f<g;++f){c=a[f];if(o)c=r("link",{charset:"utf-8","class":"lazyload",href:c,rel:"stylesheet",type:"text/css"});else{c=
+r("script",{charset:"utf-8","class":"lazyload",src:c});c.async=false}if(e.ie)c.onreadystatechange=function(){var t=this.readyState;if(t==="loaded"||t==="complete"){this.onreadystatechange=null;n()}};else if(o&&(e.gecko||e.webkit))if(e.webkit){p.urls[f]=c.href;u()}else setTimeout(n,50*g);else c.onload=c.onerror=n;q.appendChild(c)}}}function u(){var b=i.css,a;if(b){for(a=v.length;a&&--a;)if(v[a].href===b.urls[0]){k("css");break}l+=1;if(b)l<200?setTimeout(u,50):k("css")}}var e,q,i={},l=0,j={css:[],js:[]},
+v=h.styleSheets;return{css:function(b,a,c,d){m("css",b,a,c,d)},js:function(b,a,c,d){m("js",b,a,c,d)}}}(this,this.document);
+var getResJS = "?t=js&1=jscript/jsglobal.min.js&2=jscript/jsbox.min.js&3=jscript/jstrack.min.js";
+var getResCSS = "?t=css&1=style.min.css";
+
+if(lz_overlay_chat_available)
+{
+    getResCSS += "&2=overlays/chat/style.min.css";
+    getResJS += "&4=overlays/chat/jscript/jsextern.min.js";
+}
+
+LazyLoad.js(lz_poll_server + "resource.php" + getResJS, function () {lz_resources[0]=true;lz_resources[1]=true;lz_resources[2]=true;lz_resources[4]=true;});
+LazyLoad.css(lz_poll_server + "resource.php" + getResCSS, function (arg) {}, '');
